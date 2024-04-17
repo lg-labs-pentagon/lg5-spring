447493d2724c0f582a012a1440cfb29df5c7c9e7
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-18 01:22:00 +0200

    [LG-41]- add jacoco plugin

--- lg5-jvm-test/build.gradle.kts
+++ lg5-jvm-test/build.gradle.kts
@@ -1,5 +1,23 @@
+plugins {
+    jacoco
+}
+
+
 dependencies {
     api(libs.jupiter)
     api(libs.mockito.core)
 }
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+    }
+}
+
+
 tasks.jar { enabled = true }
