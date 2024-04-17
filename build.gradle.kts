b39e2d0105e73fb5f76fb11c744621c53f660810
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-17 20:08:30 +0200

    [LG-41]- base config

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    `java-library`
+    `maven-publish`
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    publishing {
+        publications {
+            create<MavenPublication>("library") {
+                from(components["java"])
+            }
+        }
+        repositories {
+            mavenLocal()
+            maven {
+                url = uri("https://maven.pkg.github.com/lg-pentagon/lg5-spring")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+
+}
+allprojects {
+    group = "com.lg5.spring"
+    version = "1.0.0-alpha"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "lg5-spring"
+include("lg5-spring-test")
+include("lg5-spring-parent")
