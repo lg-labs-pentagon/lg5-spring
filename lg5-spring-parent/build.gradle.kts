209ff1aab91f9d368b37e076283aca7695e7fc3e
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-19 18:11:28 +0200

    [LG-41]- try spring plugin

--- lg5-spring-parent/build.gradle.kts
+++ lg5-spring-parent/build.gradle.kts
@@ -1,10 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    id("org.springframework.boot") version "3.2.5" apply false
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+}
 
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-    api(libs.springboot.starter.parent)
-    api(libs.springboot.logging)
-    api(libs.guava)
-    api(libs.lombok)
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 tasks.jar { enabled = true }
