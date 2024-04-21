a9be5f8f14fcab11403dcdca3819cd08fbde66b3
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-21 14:50:58 +0200

    [LG-41]- add task jar to  kafka data

--- lg5-spring-kafka/lg5-spring-kafka-config-data/build.gradle.kts
+++ lg5-spring-kafka/lg5-spring-kafka-config-data/build.gradle.kts
@@ -3,18 +3,10 @@ plugins {
     alias(libs.plugins.springboot.plugin) apply false
     alias(libs.plugins.spring.dependency.management)
 }
-
-group = "com.lg5.spring.kafka.config.data"
-version = "1.0.0-alpha"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(libs.springboot.starter)
     implementation(libs.lombok)
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
