8c137da3aae6d422cbfdc16f9c72606454e11042
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-24 01:32:30 +0200

    [LG-41]- add a set part 5 to bootstrapServers, try lib with properties

--- lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
+++ lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
@@ -1,14 +1,31 @@
 package com.lg5.spring.kafka.config.data
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "kafka-config")
 open class KafkaConfigData {
+    @Value("\${bootstrap-servers}")
     lateinit var bootstrapServers: String
+
+    @Value("\${schema-registry-url-key}")
     lateinit var schemaRegistryUrlKey: String
+
+    @Value("\${schema-registry-url}")
     lateinit var schemaRegistryUrl: String
+
+    @Value("\${num-of-partitions}")
     lateinit var numOfPartitions: String
-    lateinit var replicationFactor: String
+
+    @Value("\${replication-factor}")
+    var replicationFactor = 0
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
 }
