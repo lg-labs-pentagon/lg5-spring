a1d6c2f2a414d12b7ce8e185e4199d60fee1c032
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-24 00:48:46 +0200

    [LG-41]- add a set part 3 to bootstrapServers, try lib with properties

--- lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
+++ lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ConfigurationProperties(prefix = "kafka-config")
 open class KafkaConfigData(
-    @Value("\${spring.kafka.bootstrap-servers}")
     var bootstrapServers: String? = null,
     var schemaRegistryUrlKey: String? = null,
-    val schemaRegistryUrl: String? = null,
-    val numOfPartitions: Int? = null,
-    val replicationFactor: Short? = null
+    var schemaRegistryUrl: String? = null,
+    var numOfPartitions: Int? = null,
+    var replicationFactor: Short? = null
 )
