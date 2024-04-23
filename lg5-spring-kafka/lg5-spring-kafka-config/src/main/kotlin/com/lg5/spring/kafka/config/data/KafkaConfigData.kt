1f8134f4aa73fb2f869b61cc438ca0d4adceb016
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-23 21:38:29 +0200

    [LG-41]- try public attrib with getter

--- lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
+++ lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConfigData.kt
@@ -1,16 +1,24 @@
 package com.lg5.spring.kafka.config.data
 
 
+import lombok.Getter
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "kafka-config")
 open class KafkaConfigData {
-    private val bootstrapServers: String? = null
+    private var bootstrapServers: String? = null
     private val schemaRegistryUrlKey: String? = null
     private val schemaRegistryUrl: String? = null
     private val numOfPartitions: Int? = null
     private val replicationFactor: Short? = null
+
+    override fun toString(): String {
+        return "KafkaConfigData(bootstrapServers=$bootstrapServers, schemaRegistryUrlKey=$schemaRegistryUrlKey, schemaRegistryUrl=$schemaRegistryUrl, numOfPartitions=$numOfPartitions, replicationFactor=$replicationFactor)"
+    }
+
+
 }
