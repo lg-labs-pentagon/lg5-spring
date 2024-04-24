39d6f836873d6e46753d161bb9de69fefef78f72
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-24 09:13:58 +0200

    [LG-41]- add more numbers properties using Kotlin's Int

--- lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConsumerConfigData.kt
+++ lg5-spring-kafka/lg5-spring-kafka-config/src/main/kotlin/com/lg5/spring/kafka/config/data/KafkaConsumerConfigData.kt
@@ -1,33 +1,24 @@
 package com.lg5.spring.kafka.config.data
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "kafka-consumer-config")
-class KafkaConsumerConfigData {
+open class KafkaConsumerConfigData {
     lateinit var keyDeserializer: String
-
     lateinit var valueDeserializer: String
-
     lateinit var autoOffsetReset: String
-
     lateinit var specificAvroReaderKey: String
-
     lateinit var specificAvroReader: String
-
     var batchListener: Boolean = false
-    var autoStartup: Boolean =  false
-
-    var concurrencyLevel: Int =0
-
-    lateinit var sessionTimeoutMs:String
-    lateinit var heartbeatIntervalMs:String
-    lateinit var maxPollIntervalMs:String
-    lateinit var pollTimeoutMs:String
-    lateinit var maxPollRecords:String
-    var maxPartitionFetchBytesDefault:Int=0
-    var maxPartitionFetchBytesBoostFactor:Int=0
-
+    var autoStartup: Boolean = false
+    var concurrencyLevel: Int = 0
+    var sessionTimeoutMs: Int = 0
+    var heartbeatIntervalMs: Int = 0
+    var maxPollIntervalMs: Int = 0
+    var pollTimeoutMs: Long = 0
+    var maxPollRecords: Int = 0
+    var maxPartitionFetchBytesDefault: Int = 0
+    var maxPartitionFetchBytesBoostFactor: Int = 0
 }
