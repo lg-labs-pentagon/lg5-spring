2117e07697fa27287034525a110cda7d4a5d7384
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-25 01:04:42 +0200

    [LG-41] - try consumer parameters

--- lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/kotlin/com/lg5/spring/kafka/consumer/KafkaConsumer.kt
+++ lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/kotlin/com/lg5/spring/kafka/consumer/KafkaConsumer.kt
@@ -4,5 +4,5 @@ import org.apache.avro.specific.SpecificRecordBase
 
 
 interface KafkaConsumer<T : SpecificRecordBase> {
-    fun receive(messages: List<T>, keys: List<String?>?, partitions: List<Int?>?, offsets: List<Long?>?)
+    fun receive(messages: List<T>, keys: List<String>, partitions: List<Int>, offsets: List<Long>)
 }
