ca71b6601069c1cd6393d633aaa94b9a088487c1
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-25 13:02:13 +0200

    [LG-41] - add consumer dynamic

--- lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/kotlin/com/lg5/spring/kafka/consumer/KafkaConsumer.kt
+++ lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/kotlin/com/lg5/spring/kafka/consumer/KafkaConsumer.kt
@@ -3,6 +3,6 @@ package com.lg5.spring.kafka.consumer
 import org.apache.avro.specific.SpecificRecordBase
 
 
-interface KafkaConsumer<T : SpecificRecordBase> {
-    fun receive(messages: List<T>, keys: List<String>, partitions: List<Int>, offsets: List<Long>)
+interface KafkaConsumer<T : SpecificRecordBase?> {
+    fun receive(messages: List<T>?, keys: List<String?>?, partitions: List<Int?>?, offsets: List<Long?>?)
 }
