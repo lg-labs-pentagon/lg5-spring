0d8fb51cb83c6e1844ef247500489d8a0e1094de
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-27 11:58:26 +0200

    [LG-41] -add avro-plugin to plugin mng into parent dependency

--- lg5-spring-parent/build.gradle.kts
+++ lg5-spring-parent/build.gradle.kts
@@ -38,10 +38,17 @@ extensions.configure<PublishingExtension> {
             pom.packaging = "pom"
             pom.properties.put("lg5.version", "\${project.parent.version}")
             pom.withXml {
-                asNode().appendNode("build").appendNode("plugins").apply {
-                    mavenCompilerPlugin()
-                    jacocoPlugin()
+                asNode().appendNode("build").apply {
+                    appendNode("pluginManagement")
+                        .appendNode("plugins").apply {
+                            avroPlugin()
+                    }
+                    appendNode("plugins").apply {
+                        mavenCompilerPlugin()
+                        jacocoPlugin()
+                    }
                 }
+
             }
         }
     }
@@ -112,3 +119,57 @@ fun Node.jacocoPlugin() {
             }
     }
 }
+
+fun Node.avroPlugin() {
+    appendNode("plugin").apply {
+        appendNode("groupId", libs.avro.plugin.get().group)
+        appendNode("artifactId", libs.avro.plugin.get().name)
+        appendNode("version", libs.versions.avro.version.get())
+
+
+        appendNode("configuration").apply {
+            appendNode("stringType", "String")
+            appendNode("enableDecimalLogicalType", "true")
+        }
+
+        appendNode("executions").apply {
+            appendNode("execution").apply {
+
+                appendNode("phase", "generate-resources")
+                appendNode("goals")
+                    .appendNode("goal", "schema")
+
+
+                appendNode("configuration").apply {
+                    appendNode("sourceDirectory", "src/main/resources/avro")
+                    appendNode("outputDirectory", "src/main/java")
+                }
+            }
+        }
+    }
+}
+/*
+
+<plugin>
+<groupId>org.apache.avro</groupId>
+<artifactId>avro-maven-plugin</artifactId>
+<version>${avro.version}</version>
+
+<configuration>
+    <stringType>String</stringType>
+    <enableDecimalLogicalType>true</enableDecimalLogicalType>
+</configuration>
+<executions>
+    <execution>
+        <phase>generate-resources</phase>
+        <goals>
+        <goal>schema</goal>
+        </goals>
+        <configuration>
+            <sourceDirectory>src/main/resources/avro</sourceDirectory>
+            <outputDirectory>src/main/java</outputDirectory>
+        </configuration>
+    </execution>
+</executions>
+
+</plugin>*/
