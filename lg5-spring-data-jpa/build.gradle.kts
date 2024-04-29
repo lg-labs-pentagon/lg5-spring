381d07cf337a71ac34486cfc26817457cbe31169
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-29 14:06:47 +0200

    [LG-41]- add spring data jpa  as lib

---
+++ lg5-spring-data-jpa/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    api(libs.springboot.data.jpa)
+    api(libs.postgresql)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,4 @@ include("lg5-common:lg5-common-domain")
 findProject(":lg5-common:lg5-common-domain")?.name = "lg5-common-domain"
 include("lg5-common:lg5-common-api")
 findProject(":lg5-common:lg5-common-api")?.name = "lg5-common-api"
+include("lg5-spring-data-jpa")
