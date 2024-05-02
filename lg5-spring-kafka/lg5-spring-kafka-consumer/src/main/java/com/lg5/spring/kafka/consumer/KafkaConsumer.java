85382d6427ac1f9069c30871b646bef31268ced1
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-05-02 22:15:56 +0200

    [LG-41]- fixed problem with encapsulation to method simple to kafka consumer

--- lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/java/com/lg5/spring/kafka/consumer/KafkaConsumer.java
+++ lg5-spring-kafka/lg5-spring-kafka-consumer/src/main/java/com/lg5/spring/kafka/consumer/KafkaConsumer.java
@@ -1,10 +1,9 @@
 package com.lg5.spring.kafka.consumer;
 
 import org.apache.avro.specific.SpecificRecordBase;
-import org.springframework.kafka.support.Acknowledgment;
 
 import java.util.List;
 
 public interface KafkaConsumer<T extends SpecificRecordBase> {
-    void receive(final List<T> messages, final List<String> keys, final List<Integer> partitions, final List<Long> offsets, final Acknowledgment acknowledgment);
+    void receive(final List<T> messages, final List<String> keys, final List<Integer> partitions, final List<Long> offsets);
 }
