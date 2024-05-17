6adf53a0b69a45690b22f29353e7f6d3bad89796
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-05-17 18:20:00 +0200

    [LG-41]- update test container libs

--- lg5-spring-testcontainers/build.gradle.kts
+++ lg5-spring-testcontainers/build.gradle.kts
@@ -1,8 +1,13 @@
+repositories {
+    mavenCentral()
+}
 dependencies{
+    implementation(libs.springboot.starter.test){
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(group = "com.vaadin.external.google", module = "android-json")
+    }
     api(libs.springboot.testcontainers)
     api(libs.testcontainers.jupiter)
     api(libs.testcontainers.postgresql)
     api(libs.testcontainers.kafka)
 }
-
-tasks.jar { enabled = true }
