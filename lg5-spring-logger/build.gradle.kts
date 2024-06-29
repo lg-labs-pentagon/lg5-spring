bccc5271c9f5438a3826b7e4dd311045773fdbb0
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-06-29 12:34:21 +0200

    Upgraded Lg5 Logger to 7.4

--- lg5-spring-logger/build.gradle.kts
+++ lg5-spring-logger/build.gradle.kts
@@ -1,13 +1,10 @@
 dependencies {
-
+    api(libs.slf4j.api)
     api(libs.logstash.logback.encoder) {
-        // <--TODO validate if it does not have change when active this comment .-- >
         exclude(group = "logback-core", module = "ch.qos.logback")
     }
-
-
-    // <-- TODO validate  the versions-->
-    api(libs.snappy.java)
+    api(libs.janino)
     api(libs.springcloud.starter.sleuth)
     api(libs.java.uuid.generator)
+    api(libs.snappy.java)
 }
