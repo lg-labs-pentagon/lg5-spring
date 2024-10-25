0f53f90ff0c61c8b3b24f186d1e2ce64a42e61fd
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-10-25 22:50:47 +0200

    [LG-71] - restore FeignClientConfiguration

--- lg5-spring-client/src/main/java/com/lg5/spring/client/config/FeignClientConfiguration.java
+++ lg5-spring-client/src/main/java/com/lg5/spring/client/config/FeignClientConfiguration.java
@@ -1,18 +1,10 @@
 package com.lg5.spring.client.config;
 
 import com.lg5.spring.client.exception.CustomErrorDecoder;
-import feign.Client;
 import feign.auth.BasicAuthRequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.cert.X509Certificate;
 
 public class FeignClientConfiguration {
 
@@ -24,34 +16,6 @@ public class FeignClientConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor(username, password);
-    }
-
-    @Bean
-    public Client feignClient() throws Exception {
-        // Trust all certificates
-        final TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-        final SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-
-        // Disable hostname verification
-        final HostnameVerifier allHostsValid = (hostname, session) -> true;
-
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-        return new Client.Default(null, null);
     }
 
     @Bean
